54520516ffe56156cff036ed4c2c62323bb1771c
Luigi <luigi.berlato@aluno.senai.br>
2025-02-17 10:25:36 -0300

    Enviando para o GitHub

---
+++ Teste.py
@@ -0,0 +1,8 @@
+a = 7
+b = int(input("Digite um numero de 1 a 10:"))
+if a == b:
+    print("Parabens, voce acertou")
+elif 1 < b > 10:
+    print("Numero invalido, tente novamente")
+else:
+    print("Que pena, voce errou")
